85925c7d1fd27f7c6dd0844b840659ef1471ef25
Yaroslav Badikov <iav.badikov@gmail.com>
2024-12-13 17:57:09 +0400

    chore(client): add eslint, ts, prettier, lint-satged, stylint configurations

---
+++ apps/client/.lintstagedrc.js
@@ -0,0 +1,7 @@
+import baseConfig from '@repo/lint-staged-config'
+
+export default {
+  ...baseConfig,
+  'src/**/*.{ts,tsx}': [() => 'npm run check-types', 'npm run lint:fix'],
+  'src/**/*.css': [() => 'npm run lint:style'],
+}
