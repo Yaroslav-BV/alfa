b3e04a70c62eee120c540e5afd19c4baa0f5cfe8
Yaroslav Badikov <iav.badikov@gmail.com>
2024-12-13 19:20:42 +0400

    chore(client): add stilelint

--- apps/client/.lintstagedrc.js
+++ apps/client/.lintstagedrc.js
@@ -3,5 +3,5 @@ import baseConfig from '@repo/lint-staged-config'
 export default {
   ...baseConfig,
   'src/**/*.{ts,tsx}': [() => 'npm run check-types', 'npm run lint:fix'],
-  'src/**/*.css': [() => 'npm run lint:style'],
+  'src/**/*.css': ['npm run lint:style'],
 }
